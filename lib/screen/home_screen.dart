12b58d1e41d232d0606b92ec77d03899630da9bd
moawiah <muawiyah454@gmail.com>
2025-06-28 21:37:05 +0300

    home screen part one

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text('Shop now'), Text('Your favorite products')],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://images.pexels.com/photos/20787/pexels-photo.jpg',
+              ),
+              radius: 20,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16,right: 16,top: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                hintText: 'Search for products',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16,right: 16),
+            child: Container(
+              height: 300,
+              child: Image.network(
+                'https://cdn.pixabay.com/photo/2020/11/13/23/49/christmas-5740350_1280.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
