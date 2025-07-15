ddcb64a43be23f1bea166a742558765f2c0749a4
moawiah <muawiyah454@gmail.com>
2025-07-15 20:05:47 +0300

    auth screen

--- lib/screen/auth/login_form_screen.dart
+++ lib/screen/auth/login_form_screen.dart
@@ -1,7 +1,7 @@
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:online_store/screen/home_screen.dart';
 import 'package:online_store/widget/custom_text_field.dart';
 
 class LoginFormScreen extends StatefulWidget {
@@ -16,33 +16,45 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
 
   TextEditingController password = TextEditingController();
   final dio = Dio();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomTextField(hintText: 'email', controller: email),
-          SizedBox(height: 20),
-          CustomTextField(hintText: 'password', controller: password),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              try {
-                await dio.post(
-                  'http://${dotenv.env['url']}:5000/users/register',
-                  data: {"email": email.text, "password": password.text},
-                );
-              } catch (e) {
-                print('error');
-              }
-            },
-            child: Text('login'),
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextField(hintText: 'email', controller: email),
+              SizedBox(height: 20),
+              CustomTextField(hintText: 'password', controller: password),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  isLoading = true;
+                  setState(() {});
+                  try {
+                    var res = await dio.post(
+                      'http://${dotenv.env['url']}:5000/users/login',
+                      data: {"email": email.text, "password": password.text},
+                    );
+                    isLoading = false;
+                    setState(() {});
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return HomeScreen();
+                        },
+                      ),
+                    );
+                  } catch (e) {}
+                },
+                child: Text('login'),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        );
   }
 }
--- lib/screen/auth/registration_screen.dart
+++ lib/screen/auth/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:online_store/screen/home_screen.dart';
 import 'package:online_store/widget/custom_text_field.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -16,41 +17,55 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   TextEditingController email = TextEditingController();
 
   TextEditingController password = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextField(hintText: 'user name', controller: username),
-            SizedBox(height: 20),
-            CustomTextField(hintText: 'email', controller: email),
-            SizedBox(height: 20),
-            CustomTextField(hintText: 'password', controller: password),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await dio.post(
-                    'http://${dotenv.env['url']}:5000/users/register',
-                    data: {
-                      "username": username.text,
-                      "email": email.text,
-                      "password": password.text,
-                    },
-                  );
-                }  catch (e) {
-                  print('error');
-                }
-              },
-              child: Text('Registration'),
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(hintText: 'user name', controller: username),
+                SizedBox(height: 20),
+                CustomTextField(hintText: 'email', controller: email),
+                SizedBox(height: 20),
+                CustomTextField(hintText: 'password', controller: password),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                      await dio.post(
+                        'http://${dotenv.env['url']}:5000/users/register',
+                        data: {
+                          "username": username.text,
+                          "email": email.text,
+                          "password": password.text,
+                        },
+                      );
+                      isLoading = false;
+                      setState(() {});
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return HomeScreen();
+                          },
+                        ),
+                      );
+                    } catch (e) {
+                      print('error: ${e}');
+                    }
+                  },
+                  child: Text('Registration'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
   }
 }
