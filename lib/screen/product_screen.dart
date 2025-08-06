9bcbb4ab51cd22871fc43c397a39a2952637c706
moawiah <muawiyah454@gmail.com>
2025-08-06 15:05:27 +0300

    home page status error handling and repair

--- lib/screen/product_screen.dart
+++ lib/screen/product_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_store/cubits/auth_cubits/auth_cubits.dart';
 import 'package:online_store/cubits/favorite_cubit/favorite_cubit.dart';
+import 'package:online_store/cubits/favorite_cubit/favorite_state.dart';
 import 'package:online_store/cubits/product_cubits/product_cubits.dart';
 import 'package:online_store/cubits/product_cubits/product_state.dart';
 import 'package:online_store/widget/product_card.dart';
@@ -22,44 +23,58 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ProductCubits, ProductState>(
-      builder: (context, state) {
-        if (state is LoadingProducts) {
-          return Center(child: CircularProgressIndicator());
-        } else if (state is GetProductsSuccessfullyState) {
-          final product = BlocProvider.of<ProductCubits>(context).productModel;
-          return GridView(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 5,
-            ),
-            children: List.generate(product.length, (index) {
-              final int user_id = int.parse(
-                "${BlocProvider.of<AuthUserCubit>(context).userModel.user_id}",
-              );
-              return Padding(
-                padding: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
-                child: ProductCard(
-                  product: product[index],
-                  user_id: user_id,
-                  icon: Icons.favorite,
-                  onPressed: () {
-                    context.read<FavoriteCubit>().addFavoriteProduct(
-                      user_id: user_id,
-                      product_id: product[index].product_id,
-                    );
-                  },
-                ),
-              );
-            }),
-          );
-        } else {
-          return Text('There are no products currently available.');
+    return BlocListener<FavoriteCubit, FavoriteState>(
+      listener: (context, state) {
+        if (state is AddToFavoritesSuccess) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Added to favorites')));
+        } else if (state is AddToFavoritesFailure) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Failed to add to favorites')));
         }
       },
+      child: BlocBuilder<ProductCubits, ProductState>(
+        builder: (context, state) {
+          if (state is LoadingProducts) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is GetProductsSuccessfullyState) {
+            final product =
+                BlocProvider.of<ProductCubits>(context).productModel;
+            return GridView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 5,
+              ),
+              children: List.generate(product.length, (index) {
+                final int user_id = int.parse(
+                  "${BlocProvider.of<AuthUserCubit>(context).userModel.user_id}",
+                );
+                return Padding(
+                  padding: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
+                  child: ProductCard(
+                    product: product[index],
+                    user_id: user_id,
+                    icon: Icons.favorite,
+                    onPressed: () {
+                      context.read<FavoriteCubit>().addFavoriteProduct(
+                        user_id: user_id,
+                        product_id: product[index].product_id,
+                      );
+                    },
+                  ),
+                );
+              }),
+            );
+          } else {
+            return Text('There are no products currently available.');
+          }
+        },
+      ),
     );
   }
 }
--- lib/widget/product_card.dart
+++ lib/widget/product_card.dart
@@ -2,9 +2,6 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:online_store/cubits/favorite_cubit/favorite_cubit.dart';
-import 'package:online_store/cubits/favorite_cubit/favorite_state.dart';
 import 'package:online_store/models/product_model.dart';
 import 'package:online_store/screen/product_details_screen.dart';
 
@@ -30,73 +27,60 @@ class _ProductCardState extends State<ProductCard> {
   Widget build(BuildContext context) {
     Uint8List imageBytes = base64Decode(widget.product.product_image);
 
-    return BlocListener<FavoriteCubit, FavoriteState>(
-      listener: (context, state) {
-        if (state is AddToFavoritesSuccess) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text('Added to favorites')));
-        } else if (state is AddToFavoritesFailure) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text('Failed to add to favorites')));
-        }
-      },
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return ProductDetailsScreen(
-                  product_name: widget.product.product_name,
-                  product_image: widget.product.product_image,
-                  information_product: widget.product.information_product,
-                  price_product: widget.product.price_product,
-                );
-              },
-            ),
-          );
-        },
-        child: Card(
-          child: ListView(
-            children: [
-              Container(
-                alignment: Alignment.centerRight,
-                width: 150,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: widget.onPressed,
-                      icon: Icon(widget.icon),
-                    ),
-
-                    Image.memory(imageBytes),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Text(widget.product.product_name),
-                ),
-              ),
-              SizedBox(height: 10),
-              ListTile(
-                title: Text(widget.product.price_product.toString()),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.shopping_cart_sharp,
-                    color: Colors.red,
-                    size: 27,
-                  ),
-                ),
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return ProductDetailsScreen(
+                product_name: widget.product.product_name,
+                product_image: widget.product.product_image,
+                information_product: widget.product.information_product,
+                price_product: widget.product.price_product,
+              );
+            },
           ),
+        );
+      },
+      child: Card(
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.centerRight,
+              width: 150,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: widget.onPressed,
+                    icon: Icon(widget.icon),
+                  ),
+
+                  Image.memory(imageBytes),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(widget.product.product_name),
+              ),
+            ),
+            SizedBox(height: 10),
+            ListTile(
+              title: Text(widget.product.price_product.toString()),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart_sharp,
+                  color: Colors.red,
+                  size: 27,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
